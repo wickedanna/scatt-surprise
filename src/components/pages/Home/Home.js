770b6f24b8991ea3330bb8b4af4d0191dc53e581
Kayla Melton <kannmelton2@gmail.com>
2020-06-01 21:29:26 -0500

    added routing to edit page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,10 +3,17 @@ import React from 'react';
 import './Home.scss';
 
 class Home extends React.Component {
+  editEvent = (e) => {
+    e.preventDefault();
+    const scatId = '12345';
+    this.props.history.push(`/edit/${scatId}`);
+  }
+
   render() {
     return (
       <div className="Home">
         <h1>Home Component</h1>
+        <button className="btn btn-info" onClick={this.editEvent}>Edit a thing</button>
       </div>
     );
   }
