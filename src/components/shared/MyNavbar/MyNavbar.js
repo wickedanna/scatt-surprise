be0c8e4a0f4f4cd19e614baa8952e40a6d1efe3c
Kayla Melton <kannmelton2@gmail.com>
2020-06-06 09:40:01 -0500

    wip

--- src/components/shared/MyNavbar/MyNavbar.js
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -1,20 +1,56 @@
 import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactsrap';
+
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
 import './MyNavbar.scss';
 
 class MyNavbar extends React.Component {
+  state = {
+    isOpen: false,
+  }
+
   logMeOut = (e) => {
     e.preventDefault();
     firebase.auth().signOut();
   }
 
+  toggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
   render() {
+    const { isOpen } = this.state;
+
+    const buildNavbar = () => {
+      const { authed }
+      if (authed)
+    }
     return (
       <div className="MyNavbar">
-        <h1>My Navbar</h1>
-        <button className="btn btn-danger" onClick={this.logMeOut}>Logout</button>
+        {/* <h1>My Navbar</h1>
+        <button className="btn btn-danger" onClick={this.logMeOut}>Logout</button> */}
+        <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Scat Surprise</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={isOpen} navbar>
+        {buildNavbar()}
+        </Collapse>
+      </Navbar>
       </div>
     );
   }
